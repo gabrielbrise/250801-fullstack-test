abbfac01b1b4e380e970510eb317a0f41d493494
gabrielbrise <gabriel_ns@live.com>
2025-08-03 19:58:12 -0300

    feat: cors for local development

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,11 @@
 import express from "express";
+import cors from "cors";
 import { getEmployment } from "./controllers/Employment";
 
 const app = express();
 const port = 3000;
+
+app.use(cors()); // <-- Allow all origins for dev
 
 app.get("/", (_req, res) => {
   res.send("Hello from Node.js + TypeScript!");
