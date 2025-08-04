5c30bf1465bceafc64df4464f06ae6672bd72a44
gabrielbrise <gabriel_ns@live.com>
2025-08-04 19:26:49 -0300

    change: swap FilterSection and HeaderSection positions

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,8 +5,8 @@ import TableSection from "./sections/TableSection";
 function App() {
   return (
     <div className="max-w-4xl mx-auto px-4 py-24">
-      <HeaderSection />
       <FilterSection />
+      <HeaderSection />
       <TableSection />
     </div>
   );
